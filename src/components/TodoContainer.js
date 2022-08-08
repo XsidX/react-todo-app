16e975bc57c08143309074f7a8c1fb2c372be7c5
iamsid <developer.sid@outlook.com>
2022-08-08 17:55:32 +0300

    add todoContainer, todoItem and todoList components

---
+++ src/components/TodoContainer.js
@@ -0,0 +1,33 @@
+import React, { PureComponent } from 'react';
+import TodosList from './TodoList';
+
+class TodoContainer extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [
+        {
+          id: 1,
+          title: 'Setup development environment',
+          completed: true,
+        },
+        {
+          id: 2,
+          title: 'Develop website and add content',
+          completed: false,
+        },
+        {
+          id: 3,
+          title: 'Deploy to live server',
+          completed: false,
+        },
+      ],
+    };
+  }
+
+  render() {
+    const { todos } = this.state;
+    return <TodosList todos={todos} />;
+  }
+}
+export default TodoContainer;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import TodoContainer from './components/TodoContainer';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <TodoContainer />
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
